2780429ba6cb6885d3599b5578b0be2a6bc46ba5
marloustolk <marloustolk@gmail.com>
2021-01-29 22:48:19 +0100

    select place

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
+places: Map<number, boolean>;
+shape: number[] = [1,3,10,11,12,20];
+boardSquareCount: number = 9;
 
+  ngOnInit() {
+    this.places = new Map<number, boolean>();
+    this.deselect();
+  }
+
+  getColor(selected: boolean): string {
+    return selected ? "rgb(122, 209, 133)" : "rgba(255, 255, 255, 0.8)";
+  }
+
+  select(key: number){
+    this.places.set(key, true);
+  }
+
+  deselect(){
+    for (let i = 1; i <= Math.pow(this.boardSquareCount, 2); i++){
+      this.places.set(i, false);
+    }
+  }
 }
